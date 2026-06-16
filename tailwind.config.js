cee5d076b14843ebcce4a111dcd7be958010ef80
Kelvinton <Oguseaaron07@gmail.com>
2026-06-16 13:15:20 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    content: ["./"],
+    theme: {
+        extend: {
+            colors: {
+                //test: "#FAFCFD"
+
+            }, 
+
+            fontFamily: {
+            sans: ['poppins', 'Inter' ,'sans-serif'],
+            serif: ['Coromorant Garamond','Playfair Display', 'Libre' ,'serif'],
+            },
+            
+        },
+        
+    },
+    plugins: [],
+};
